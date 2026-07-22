05f38ed55bcf7a214af15431c1ce572a955b0041
MG_Kalinin <1953115@gmail.com>
2026-07-22 20:39:32 +0300

    added complited task

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-# https://cscircles.cemc.uwaterloo.ca/visualize
-
 import sys
-import heapq
-from collections import deque
 
-#
 
 def main():
+    """
+    Пример ввода и вывода числа n, где -10^9 < n < 10^9:
+    n = int(input())
+    print(n)
+    """
+    # a=int(input())
+    # b=int(input())
+    # c=int(input())
+    a=3
+    b=4
+    c=5
 
+    if (a+b)>c and (a+c)>b and (b+c)>a:
+        print('YES')
+    else:
+        print('NO')
 
 
 if __name__ == '__main__':

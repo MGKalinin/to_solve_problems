93a8b0f6556826a43dffe129c2fb16c823106321
MG_Kalinin <1953115@gmail.com>
2026-07-26 13:27:11 +0300

    №5

--- main.py
+++ main.py
@@ -7,20 +7,7 @@ def main():
     n = int(input())
     print(n)
     """
-    n=int(input())#28656
-    # n=1
-    if n==1:
-        print(n)
-        return
-    temp=[0]*n
-    # print(len(temp))
-    temp[0]=1
-    temp[1]=1
-    # t[i]=t[i-1]+t[i-2]
-    for i in range(2,n):
-        temp[i]=temp[i-1]+temp[i-2]
-    # print(temp)
-    print(sum(temp))
+
 
 if __name__ == '__main__':
     main()

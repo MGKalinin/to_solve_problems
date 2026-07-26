d6a5d233d165cc32ab7ae1bc407f2c0b6a856b88
MG_Kalinin <1953115@gmail.com>
2026-07-26 10:08:34 +0300

    task №4

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ def main():
     n = int(input())
     print(n)
     """
-    temp=list(map(int,input().split()))
-    # temp=[1 ,5 ,1 ,5 ,1]
-    count=0
-    for i in range(1,len(temp)-1):
-        if temp[i]>temp[i-1] and temp[i]>temp[i+1]:
-            count+=1
-    print(count)
-
+    n=int(input())#28656
+    # n=1
+    if n==1:
+        print(n)
+        return
+    temp=[0]*n
+    # print(len(temp))
+    temp[0]=1
+    temp[1]=1
+    # t[i]=t[i-1]+t[i-2]
+    for i in range(2,n):
+        temp[i]=temp[i-1]+temp[i-2]
+    # print(temp)
+    print(sum(temp))
 
 if __name__ == '__main__':
     main()

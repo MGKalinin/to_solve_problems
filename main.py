5c6394a9f8d381eec85eeb50ad7003c79f826300
MG_Kalinin <1953115@gmail.com>
2026-08-01 18:51:55 +0300

    № 5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+#6. OpenCalculator
 
 def main():
     """
@@ -7,26 +8,13 @@ def main():
     n = int(input())
     print(n)
     """
-    # D=b**2-4*a*c
-    #D>0 x1=(-b-math.sqr(D))/(2*a) x2=(-b+math.sqr(D))/(2*a)
-    #D==0 x=-(b/2*a)
-    #D<0 нет корней
-    a,b,c=map(int,input().split())
-    # a,b,c=6 ,-5 ,-8
-    D = (b ** 2) - (4 * a * c)
-    match D:
-        case _ if D==0:
-            x=-(b/(2*a))
-            print(1)
-            print(x)
-        case _ if D>0:
-            x1=(-b-(D**0.5))/(2*a)
-            x2=(-b+(D**0.5))/(2*a)
-            print(2)
-            print(f'{min(x1,x2):.10f} {max(x1,x2):.10f}')
-        case _ if D<0:
-            print(0)
+    first = set(map(str,input().split()))
+    second = set([i for i in input()])
+    # print(first)
+    # print(second)
 
+    ans=second-first
+    print(len(ans))
 
 if __name__ == '__main__':
     main()
